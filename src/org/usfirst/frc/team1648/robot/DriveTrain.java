1470c08315f3272ddfd94527bd9d1b7b6b922ea8
Benjamin Borthwick <benjabee@comcast.net>
2018-06-28 14:10:36 -0400

    Created system to stop the drive train if a motor is disconnected

--- src/org/usfirst/frc/team1648/robot/DriveTrain.java
+++ src/org/usfirst/frc/team1648/robot/DriveTrain.java
@@ -8,19 +8,36 @@ public class DriveTrain {
 	private TalonSRX left, right;
 	private VictorSPX left1, left2, right1, right2;
 	public DriveTrain() {
-		left = new TalonSRX(1);
-		right = new TalonSRX(1);
+		left = new TalonSRX(0);
+		right = new TalonSRX(10);
 		left1 = new VictorSPX(1);
-		left2 = new VictorSPX(1);
-		right1 = new VictorSPX(1);
-		right2 = new VictorSPX(1);
+		left2 = new VictorSPX(2);
+		right1 = new VictorSPX(11);
+		right2 = new VictorSPX(12);
 		left1.follow(left);
 		left2.follow(left);
 		right1.follow(right);
 		right2.follow(right);
 	}
 	public void setSpeed(double left, double right) {
-		this.left.set(ControlMode.PercentOutput, left);
-		this.right.set(ControlMode.PercentOutput, left);
+		try {
+			this.left.set(ControlMode.PercentOutput, left);
+			this.left1.getDeviceID();
+			this.left2.getDeviceID();
+		} catch (NullPointerException e) {
+			this.left.set(ControlMode.PercentOutput, 0);
+			this.right.set(ControlMode.PercentOutput, 0);
+			System.out.println("One of the motors on the left side of the robot isn't connected to it's port.");
+		}
+		try {
+			this.right.set(ControlMode.PercentOutput, right);
+			this.right1.getDeviceID();
+			this.right2.getDeviceID();
+		} catch (NullPointerException e) {
+			this.right.set(ControlMode.PercentOutput, 0);
+			this.left.set(ControlMode.PercentOutput, 0);
+			System.out.println("One of the motors on the right side of the robot isn't connected to it's port.");
+
+		}
 	}
 }
